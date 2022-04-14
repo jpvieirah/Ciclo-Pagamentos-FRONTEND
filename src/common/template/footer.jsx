1c9d3a304d5031b90c109e1ea84e98d11fe30943
jpvieirah <jp201101@gmail.com>
2022-04-14 12:26:34 -0300

    Componente Rodapé

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2022
+            <a href="https://github.com/jpvieirah" target='_blank'> João </a>.
+        </strong>
+    </footer>
+)
