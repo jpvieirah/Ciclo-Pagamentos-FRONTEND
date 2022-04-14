5b01957b20335cc055418b717e9fe5b960ac1d3c
jpvieirah <jp201101@gmail.com>
2022-04-14 12:27:36 -0300

    continuação

--- src/common/template/footer.jsx
+++ src/common/template/footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default props => (
-    <footer className="main-footer">
+    <footer className="main-footer" >
         <strong>
             Copyright &copy; 2022
             <a href="https://github.com/jpvieirah" target='_blank'> João </a>.
