1e91ae53fc650f24bbb437959c00baddc88c46e4
jpvieirah <jp201101@gmail.com>
2022-04-19 12:21:29 -0300

    BillingCycleForm com Redux-Form (Parte 02)

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -12,6 +12,7 @@ import TabContent from "../common/tab/tabContent";
 import { selectTab, showTabs } from "../common/tab/tabActions";
 import List from "./billingCycleList";
 import Form from './billingCycleForm'
+import { create } from './billingCycleActions'
 
 class BillingCycle extends Component {
 
@@ -37,7 +38,7 @@ class BillingCycle extends Component {
                         <List />
                     </TabContent>
                     <TabContent id='tabCreate'>
-                        <Form />
+                        <Form onSubmit={this.props.create}/>
                     </TabContent>
                     <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
                     <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
@@ -49,5 +50,5 @@ class BillingCycle extends Component {
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({selectTab, showTabs}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab, showTabs, create}, dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycle)
